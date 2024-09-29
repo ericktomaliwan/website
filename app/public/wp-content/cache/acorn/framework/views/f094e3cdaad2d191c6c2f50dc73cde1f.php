9c28c774921834dd65477fdbc4b433b12da0d110
ericktomaliwan <ericktoma@outlook.com>
2024-09-29 15:59:09 +1000

    KAN-8 Install new theme

---
+++ app/public/wp-content/cache/acorn/framework/views/f094e3cdaad2d191c6c2f50dc73cde1f.php
@@ -0,0 +1,4 @@
+<footer class="content-info">
+  <?php (dynamic_sidebar('sidebar-footer')); ?>
+</footer>
+<?php /**PATH /Users/ericktomaliwan/Local Sites/erick-website-2024/app/public/wp-content/themes/ericktomaliwan/resources/views/sections/footer.blade.php ENDPATH**/ ?>
