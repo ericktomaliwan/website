7c2e59596c79e1b5a743d640c57d638807a4e9fb
ericktomaliwan <ericktoma@outlook.com>
2024-11-09 21:49:34 +1100

    update plugins nov 9 2

--- app/public/wp-content/plugins/kadence-blocks/dist/blocks-column.asset.php
+++ app/public/wp-content/plugins/kadence-blocks/dist/blocks-column.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('kadence-components', 'kadence-helpers', 'kadence-icons', 'lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '572b7c7709b87b9cee49');
+<?php return array('dependencies' => array('kadence-components', 'kadence-helpers', 'kadence-icons', 'lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '1a2b367b75e86e52814a');
