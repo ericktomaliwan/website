2a6033a4f4891c8766ff860b89991e6dcbf4f56d
ericktomaliwan <ericktoma@outlook.com>
2025-04-20 16:12:18 +1000

    KAN-12 more changes

--- app/public/wp-content/themes/ericktomaliwan/resources/scripts/app.js
+++ app/public/wp-content/themes/ericktomaliwan/resources/scripts/app.js
@@ -4,7 +4,17 @@ import domReady from '@roots/sage/client/dom-ready';
  * Application entrypoint
  */
 domReady(async () => {
-  // ...
+  const hamburgerButton = document.getElementById('hamburgerButton');
+  const mobileMenu = document.getElementById('mobileMenu');
+  const popover = document.getElementById('popover-backdrop');
+
+  if (hamburgerButton && mobileMenu) { // Check if elements exist
+    hamburgerButton.addEventListener('click', () => {
+      mobileMenu.classList.toggle('hidden');
+      mobileMenu.classList.toggle('fade');
+      popover.classList.toggle('hidden');
+    });
+  }
 });
 
 /**
--- app/public/wp-content/themes/ericktomaliwan/resources/views/partials/page-header.blade.php
+++ app/public/wp-content/themes/ericktomaliwan/resources/views/partials/page-header.blade.php
@@ -1,5 +1,7 @@
 <div class="page-header">
+  <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
   @if ( !is_page() )
     <h1 class="inline-block text-2xl sm:text-3xl font-extrabold">{!! $title !!}</h1>
   @endif
+</div>
 </div>
